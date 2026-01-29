f47a97521b6ee5c3abb7070d06782d302c2539ef
Unknownaod <unknown@dona.pw>
2026-01-29 03:21:04 -0500

    Create adminAuth.js

---
+++ middleware/adminAuth.js
@@ -0,0 +1,7 @@
+// middleware/adminAuth.js
+module.exports = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    return next();
+  }
+  return res.status(403).json({ error: 'Access denied. Admins only.' });
+};
