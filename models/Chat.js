da03b66a1ecb213818eaec91d18026878b4162ae
Unknownaod <unknown@dona.pw>
2026-02-08 22:57:24 -0500

    Create Chat.js

---
+++ models/Chat.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const ChatSchema = new mongoose.Schema({
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }],
+  messages: [MessageSchema],
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Chat', ChatSchema);
